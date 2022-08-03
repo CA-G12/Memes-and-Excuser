506f68513eac0a941ffd7f7257714bf151ee19c9
MohammedOmar123 <mohammedbalousha60@gmail.com>
2022-08-03 14:42:54 +0300

    At First,I added 5 images for categories then I modifed some elemnets in html file and I add CSS Styles for the page related #4

--- Javascripts/excuser/logic.js
+++ Javascripts/excuser/logic.js
@@ -1,5 +1,5 @@
 fetchApi("https://excuser.herokuapp.com/v1/excuse/500", function (data) {
-   extractCategories(data); // to extract all categories from the objects to display them later
+  extractCategories(data); // to extract all categories from the objects to display them later
 });
 function extractCategories(data) {
   let categories = [];
@@ -9,6 +9,29 @@ function extractCategories(data) {
       categories.push(excuse.category);
     }
   });
-  return categories ; 
 }
 
+
+function findExcusesFromCategories(category, data) {
+  let excuses = data.filter((excuse) => {
+    return excuse.category === category;
+  });
+  return createExcusesViews(excuses);
+}
+
+function selectImage(categoryName) {
+  const images = [
+    "../images/college.jpg",
+    "../images/family.webp",
+    "../images/office.jpg",
+    "../images/party.gif",
+    "../images/children.webp",
+  ];
+
+  return images.filter((image) => {
+    return (
+      image.split("/").slice(2).join("").split(".").splice(0, 1)[0] ==
+      categoryName
+    );
+  });
+}
