40e0a128feac1cbedb1dca9c2061761916774b36
MohammedOmar123 <mohammedbalousha60@gmail.com>
2022-08-03 16:45:59 +0300

    replace 2 photos

--- Javascripts/excuser/logic.js
+++ Javascripts/excuser/logic.js
@@ -28,7 +28,7 @@ function selectImage(categoryName) {
     "../images/family.webp",
     "../images/office.jpg",
     "../images/party.gif",
-    "../images/children.webp",
+    "../images/children.jpg",
   ];
 
   return images.filter((image) => {
