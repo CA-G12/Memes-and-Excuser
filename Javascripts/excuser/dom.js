1162a92b88b4f7ee462898671bbc7e2a05ada0f2
MohammedOmar123 <mohammedbalousha60@gmail.com>
2022-08-03 15:36:18 +0300

    solve confilect

--- Javascripts/excuser/dom.js
+++ Javascripts/excuser/dom.js
@@ -2,26 +2,26 @@ const containers = createPointers();
 const container = containers[0]; // assign pointers
 const excuseContainer = containers[1];
 function createPointers() {
-    return [
-        document.getElementById("container"),
-        document.getElementById("excuseContainer"),
-    ];
+  return [
+    document.getElementById("container"),
+    document.getElementById("excuseContainer"),
+  ];
 }
 // createCategoryView(category) to display categories on the screen
 function createCategoryView(category, data) {
-    const paraCategory = createViews(category); // to create p element and set content for it
-    paraCategory.addEventListener(
-        "click",
-        () => {
-            findExcusesFromCategories(category, data);
-        },
-        false
-    );
-    container.append(paraCategory);
+  const paraCategory = createViews(category); // to create p element and set content for it
+  paraCategory.addEventListener(
+    "click",
+    () => {
+      findExcusesFromCategories(category, data);
+    },
+    false
+  );
+  container.append(paraCategory);
 }
 // generic task to create p elements
 function createViews(text) {
-    const para = document.createElement("p");
-    para.textContent = text;
-    return para;
+  const para = document.createElement("p");
+  para.textContent = text;
+  return para;
 }
