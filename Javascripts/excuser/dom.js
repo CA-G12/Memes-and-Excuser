7c5e9a4c165381880b50b7a8c163854887192dbf
MohammedOmar123 <mohammedbalousha60@gmail.com>
2022-08-03 09:23:44 +0300

    I created a categoryView,Pointers and createViews Functions for diplay category names in the screen

--- Javascripts/excuser/dom.js
+++ Javascripts/excuser/dom.js
@@ -1,4 +1,12 @@
-
+const containers = createPointers();
+const container = containers[0]; // assign pointers
+const excuseContainer = containers[1];
+function createPointers() {
+    return [
+        document.getElementById("container"),
+        document.getElementById("excuseContainer"),
+    ];
+}
 // createCategoryView(category) to display categories on the screen
 function createCategoryView(category, data) {
     const paraCategory = createViews(category); // to create p element and set content for it
@@ -10,4 +18,10 @@ function createCategoryView(category, data) {
         false
     );
     container.append(paraCategory);
+}
+// generic task to create p elements
+function createViews(text) {
+    const para = document.createElement("p");
+    para.textContent = text;
+    return para;
 }
