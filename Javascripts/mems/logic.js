91b471091fe6be68bb93eb92295598ee0faad3ba
salsabeelomar <omarsalsabeel65@gmail.com>
2022-08-03 11:54:44 +0300

    hide some API items relates #22

--- Javascripts/mems/logic.js
+++ Javascripts/mems/logic.js
@@ -5,7 +5,7 @@ function fetchFromApi(data,cb){
 
     const dataMeme= data.data.memes
     for (let nummeme=0; nummeme<=50;nummeme++ ){
-        if (nummeme!=2){
+        if (nummeme!=2&&nummeme!=21&& nummeme!=0){
           let memeImg= dataMeme[nummeme].url 
           let memeName=dataMeme[nummeme].name
           let boxCount=dataMeme[nummeme].box_count
