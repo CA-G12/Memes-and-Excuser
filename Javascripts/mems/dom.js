f4657c449b41292e886da19a6e257405c632dee1
salsabeelomar <omarsalsabeel65@gmail.com>
2022-08-03 10:39:41 +0300

    create dom for the fetched item from api relates #17

--- Javascripts/mems/dom.js
+++ Javascripts/mems/dom.js
@@ -1,3 +1,6 @@
+const memeSection = document.getElementsByClassName("meme-container")[0]
+
+fetchApi("https://api.imgflip.com/get_memes",(data)=>{fetchFromApi(data,memeDom)})
 
 function memeDom(imgurl, name , boxCount ) {
    let memeDiv = document.createElement("div")
@@ -8,10 +11,14 @@ function memeDom(imgurl, name , boxCount ) {
 
    let memeName = document.createElement("h3")
    memeName.classList.add("meme-name")
+   memeName.textContent=name
    
    let memeBoxCount= document.createElement("span")
    memeBoxCount.classList.add("meme-box-count")
+   memeBoxCount.textContent=boxCount
    
    memeDiv.append(memeImg,memeName,memeBoxCount)
-
+   memeSection.appendChild(memeDiv)
 }
+
+   
